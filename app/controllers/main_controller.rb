623ea764c89424c024010323099d2ae7775738b8
sndpjagtap <sajagtap@iu.edu>
2022-02-13 18:45:35 +0000

    flash messages

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,7 +1,8 @@
 class MainController < ApplicationController
 
     def index
-    
+        flash.now[:notice]="logged in successfully"
+        flash.now[:alert]="Invalid email or password"
     end
 
 end
